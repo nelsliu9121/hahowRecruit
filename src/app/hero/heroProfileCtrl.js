0f9c9c02c62973f067543cc2f1efb57e2a250ce4
Nelson Liu <nelsliu9121@gmail.com>
2016-11-19 16:44:54 +0800

    making hero profile working

--- src/app/hero/heroProfileCtrl.js
+++ src/app/hero/heroProfileCtrl.js
@@ -1,8 +1,33 @@
 export class heroProfileCtrl {
-  constructor($state, Hero) {
+  constructor($state, $mdToast, Hero) {
+    this.Hero = Hero;
     this.heroId = $state.params.heroId;
-    Hero.profile({heroId: this.heroId}, data => {
-      this.profile = data;
+    this.$mdToast = $mdToast;
+    this.remaining = 0;
+    this.profile = Hero.profile({heroId: this.heroId});
+  }
+
+  increase(key) {
+    if (this.remaining > 0) {
+      this.profile[key]++;
+      this.remaining--;
+    }
+  }
+
+  decrease(key) {
+    if (this.profile[key] > 0) {
+      this.profile[key]--;
+      this.remaining++;
+    }
+  }
+
+  save() {
+    if (this.remaining !== 0) {
+      return false;
+    }
+    this.profile.$save({heroId: this.heroId}, () => {
+      this.profile = this.Hero.profile({heroId: this.heroId});
+      this.$mdToast.showSimple('Hero Profile Saved');
     });
   }
 }
